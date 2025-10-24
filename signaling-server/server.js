daab8c8349153e7b0fa1a7f37abe5c9f6fcc56c5
abhishekpuranipd <abhishekpuranikpd@gmail.com>
2025-10-24 11:43:32 +0530

    #changes added for chat

--- signaling-server/server.js
+++ signaling-server/server.js
@@ -27,5 +27,39 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => console.log("disconnected:", socket.id));
 });
 
+// ... EXISTING CODE above ...
+
+io.on("connection", (socket) => {
+  console.log("connected:", socket.id);
+
+  // -- Existing video signaling events here --
+
+  // -- New: join chat room (can reuse join-room) --
+  socket.on("join-chat-room", ({ roomId, userId }) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-joined-chat", { userId });
+  });
+
+  // -- New: chat message event --
+  socket.on("chat-message", ({ roomId, message, userId, type }) => {
+    // message: text or file info, type: 'text'/'image'/'video'/'audio'
+    io.to(roomId).emit("chat-message", { userId, message, type, time: Date.now() });
+  });
+
+  // -- Typing indicator --
+  socket.on("typing", ({ roomId, userId }) => {
+    socket.to(roomId).emit("typing", { userId });
+  });
+
+  // -- Delete message --
+  socket.on("delete-message", ({ roomId, messageId, userId }) => {
+    io.to(roomId).emit("delete-message", { messageId, userId });
+  });
+
+  socket.on("disconnect", () => console.log("disconnected:", socket.id));
+});
+
+
+
 const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Signaling server running on ${PORT}`));
